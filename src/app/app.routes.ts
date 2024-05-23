866e98f71af4f2420a09019b88e8742facf288c9
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2024-05-23 13:29:06 -0300

    estilização simples

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
+import { NewComponent } from './components/pages/new/new.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent }
+    {path: 'about', component: AboutComponent },
+    {path: 'moments/new', component: NewComponent}
 ];
