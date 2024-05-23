20b739897e848be86e1681a6b7ae81de7ae327d3
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2024-05-23 14:53:11 -0300

    criação services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLinkActive ,RouterLink, RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterLinkActive,
+    RouterLink,
+    RouterOutlet,
+    FooterComponent,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'moments';
