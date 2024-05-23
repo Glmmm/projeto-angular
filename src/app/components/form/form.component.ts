81ff87a516ea4ee33fb2a5193875633927dd5430
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2024-05-23 14:02:25 -0300

    criação formulario

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
-
+  @Input() btnText!: string;
 }
--- src/app/components/pages/new/new.component.ts
+++ src/app/components/pages/new/new.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormComponent } from '../../form/form.component';
 
 @Component({
   selector: 'app-new',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './new.component.html',
   styleUrl: './new.component.css'
 })
 export class NewComponent {
-
+  btnText = "Compartilhar";
 }
