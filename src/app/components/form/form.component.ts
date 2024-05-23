0049747fda263d6e4a53f4a794186e0b0c68f04e
Guilherme Papa de Souza <64666460+Glmmm@users.noreply.github.com>
2024-05-23 17:46:37 -0300

    correção bug forms

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
   @Input() btnText!: string;
+
+  momentForm = new FormGroup({
+    id: new FormControl(''),
+    title: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    image: new FormControl(''),
+  });
+
+  submit() {
+    console.log('enviou');
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+  get description() {
+    return this.momentForm.get('description')!;
+  }
 }
